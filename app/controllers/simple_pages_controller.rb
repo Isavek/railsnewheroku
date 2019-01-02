20d50727cc834ebfd92b4e8a8d0f583e985d12d6
“Ivan <isavek@icloud.com>
2019-01-01 23:32:24 -0600

    4.5 carousel and partials

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,5 +1,6 @@
 class SimplePagesController < ApplicationController
   def landing_page
-    @featured_product = Product.first
+    @featured_product=Product.first
+    @products = Product.limit(4)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
+  resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
