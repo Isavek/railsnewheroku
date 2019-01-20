c79914439dd92cc492ea121741a1837a20de35d8
“Ivan <isavek@icloud.com>
2019-01-19 20:15:01 -0600

    5.3 mailgun

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :authentication => :plain,
+  :address => "app.mailgun.com",
+  :port => 587,
+  :domain => "bike-world.herokuapp.com",
+  :user_name => "postmaster@MYDOMAIN.mailgun.com",
+  :password => "MYPASSWORD"
+}
