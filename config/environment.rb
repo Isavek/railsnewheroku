cda127fbf0262eee05ece154b2aa116d2a3b24fb
“Ivan <isavek@icloud.com>
2019-01-19 21:06:26 -0600

    5.3 sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :authentication => :plain,
-  :address => "app.mailgun.com",
-  :port => 587,
-  :domain => "bike-world.herokuapp.com",
-  :user_name => "postmaster@MYDOMAIN.mailgun.com",
-  :password => "MYPASSWORD"
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'heroku.com',
+  enable_starttls_auto: true
 }
