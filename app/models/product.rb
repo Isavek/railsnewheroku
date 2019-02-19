adaf75b632aa978228e9a59524d53b7b3c1c7fbc
“Ivan <isavek@icloud.com>
2019-02-18 23:31:38 -0600

    5.7 added Comment Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,18 @@
 class Product < ApplicationRecord
  has_many :orders
- def self.search(search_term)
-  if Rails.env.development?
-    @products = Product.where('name LIKE ?', "%#{search_term}%")
-  elsif Rails.env.production?
-    @products = Product.where('name ilike ?', "%#{search_term}%")
+ has_many :comments, dependent: :destroy
+  def self.search(search_term)
+    if Rails.env.development?
+       @products = Product.where('name LIKE ?', "%#{search_term}%")
+    elsif Rails.env.production?
+       @products = Product.where('name ilike ?', "%#{search_term}%")
   end
+  def highest_rating_comment
+  		comments.rating_desc.first
+  	end
+
+  	def lowest_rating_comment
+  		comments.rating_asc.first
+  	end
 end
 end
