42497d9827988e139ac6d61879d044762eaafbb9
“Ivan <isavek@icloud.com>
2019-03-19 21:23:00 -0500

    5.10 Admin Role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,15 @@ class Ability
     can :manage, User, id: user.id
     can :read, Order, user_id: user.id
 end
+
+if user.admin?
+  can :manage, :all
+else
+  can [:show, :edit, :update], User, id: user.id
+  can [:show, :index], Order, user_id: user.id
+  can :create, Comment, user_id: user.id
+  can [:show, :index], Product
+end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
