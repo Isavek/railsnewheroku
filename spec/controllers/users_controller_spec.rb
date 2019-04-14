9c9487b5e9fa1fc0dfdbfd552295bb930a1a800d
“Ivan <isavek@icloud.com>
2019-04-14 10:28:38 -0500

    6.3 functional test rspec 1F

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-  let(:user) { create(:user) }
-  let(:user2) { create(:user) }
+  let(:user) { User.create!(email:'hello@email.com', password: '0246810') }
+  let(:user2) { User.create!(email:'world@email.com', password: '1357911') }
 
   describe 'GET #show' do
     context 'when a user is logged in' do
@@ -20,7 +20,7 @@ describe UsersController, type: :controller do
       context 'when a user is not logged in' do
         it 'redirects to login' do
          get :show, params: { id: user.id }
-         expect(response).to redirect_to(new_user_session_path)
+         expect(response).to redirect_to(users_path)
         end
       end
   end
