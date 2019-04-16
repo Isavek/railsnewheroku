12358a1a863b0b606115bf7ab0fff03c4925ecc7
“Ivan <isavek@icloud.com>
2019-04-15 22:51:14 -0500

    6.4 added factory bot

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,27 +1,38 @@
+
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-  let(:user) { User.create!(email:'hello@email.com', password: '0246810') }
-  let(:user2) { User.create!(email:'world@email.com', password: '1357911') }
-
-  describe 'GET #show' do
-    context 'when a user is logged in' do
-      before do
-        sign_in user
-      end
-
-      it 'loads correct user details' do
-        get :show, params: {id: user.id}
-        expect(response).to be_ok
-        expect(assigns(:user)).to eq user
-      end
+    before do
+        @user1 = FactoryBot.create(:user)
+        @user2 = FactoryBot.create(:user)
     end
 
-      context 'when a user is not logged in' do
-        it 'redirects to login' do
-         get :show, params: { id: user.id }
-         expect(response).to redirect_to(new_user_session_path)
+    describe "Get #show" do
+        context "when a user is logged in" do
+
+            before do
+                sign_in @user1
+            end
+
+            it 'loads correct user details' do
+                get :show, params: {id: @user1.id}
+                expect(response).to be_ok
+                expect(assigns(:user)).to eq @user1
+            end
         end
-      end
-  end
+
+            it "tries accessing first users show page" do
+                get :show, params: {id: @user2.id}
+                expect(response).to have_http_status(302)
+                expect(response).to redirect_to(new_user_session_path)
+            end
+
+            context 'when a user is not logged in' do
+                it 'redirects to login' do
+                    get :show, params: {id: @user1.id}
+                    expect(response).to redirect_to(new_user_session_path)
+                end
+            end
+    end
+
 end
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,21 @@
+
+FactoryBot.define do
+    sequence(:email) {|n| "test#{n}@example.com"}
+
+    factory :user do
+      email
+      password 'password'
+      first_name 'John'
+      last_name 'Smith'
+      admin false
+    end
+
+    factory :admin, class: User do
+      email
+      password 'password'
+      admin true
+      first_name 'Jane'
+      last_name 'Smith'
+    end
+
+  end
