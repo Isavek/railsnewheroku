f81f2c05f723737608a000e5f0e9b4a2d8611e4e
“Ivan <isavek@icloud.com>
2019-04-19 00:23:41 -0500

    6.6 poste

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,31 @@
+class PaymentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+  @product = Product.find(params[:product_id])
+  @user = current_user
+  token = params[:stripeToken]
+  begin
+    charge = Stripe::Charge.create(
+       :amount => (@product.price * 100).to_i,
+       :currency => "usd",
+       :source => token,
+       :description => params[:stripeEmail],
+       :receipt_email => params[:stripeEmail]
+     )
+
+  if charge.paid
+    Order.create(product_id: @product.id,user_id: @user.id, total: @product.price)
+    flash[:success] = "Your payment was processed successfully"
+  end
+
+  rescue Stripe::CardError => e
+    # The card has been declined
+    body = e.json_body
+    err = body[:error]
+    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+  redirect_to orders_path(@product), notice: "Thank you for your purchase."
+  end
+
+end
