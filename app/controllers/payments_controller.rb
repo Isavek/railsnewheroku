0e3c1e3811acf71cdac5189d6ebcc3cf1597815c
“Ivan <isavek@icloud.com>
2019-04-22 00:06:25 -0500

    6.6 creating payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,9 +2,10 @@ class PaymentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-  @product = Product.find(params[:product_id])
+  @order = params[:order_ids]
   @user = current_user
   token = params[:stripeToken]
+  # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
        :amount => (@product.price * 100).to_i,
@@ -25,7 +26,7 @@ class PaymentsController < ApplicationController
     err = body[:error]
     flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
-  redirect_to orders_path(@product), notice: "Thank you for your purchase."
+  redirect_to product_path(@product), notice: "Thank you for your purchase."
   end
 
 end
