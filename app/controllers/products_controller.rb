28623f772e3832282f80b46c1ba69aae48568e7c
“Ivan <isavek@icloud.com>
2019-05-04 00:35:29 -0500

    6.12 redis no method error incr

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,7 @@ class ProductsController < ApplicationController
   def show
 
     @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
+    @product.viewed!
 
   end
 
