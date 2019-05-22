65b1224b110df673df869eb2e3660b5eb7628c6e
“Ivan <isavek@icloud.com>
2019-05-21 22:57:34 -0500

    added action cable

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   get 'simple_pages/landing_page'
   post 'simple_pages/thank_you'
   post 'payments/create'
+
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
